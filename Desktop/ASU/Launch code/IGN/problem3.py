2c4e9268f6dedab8564f4f7d6c86b71aa9707514
Jesse Hanner <jahanner7@gmail.com>
2017-03-03 13:49:52 -0600

    On the right track

--- Desktop/ASU/Launch code/IGN/problem3.py
+++ Desktop/ASU/Launch code/IGN/problem3.py
@@ -2,6 +2,7 @@ import random
 # Define numbers to choose from
 numbers = ('0','1','2','3','4','5','6','7','8','9')
 # Define empty list
+area = 9
 grid = []
 # Make rows with len 3
 for row in range(3):
@@ -15,5 +16,17 @@ for row in range(3):
 def print_grid(grid):
     for row in grid:
         print' '.join(row)
-
+# Maybe make a list with all possible grid positions as variables and iterate through it...
 print_grid(grid)
+row1 = grid[0]
+row2 = grid[1]
+row3 = grid[2]
+row1_horz = False
+print(row1)
+while row1_horz == False:
+    if row1[0] + row1[1] or row1[0] + row1[1] + row1[2] == area:
+        row1_horz = True
+print(row1_horz)
+# for col in row1:
+#     col += col
+# print(col)
